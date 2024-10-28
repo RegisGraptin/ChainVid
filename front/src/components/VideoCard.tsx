059868ea19d789235208bca18dccbf222de66f62
rere <regis-graptin@sfr.fr>
2024-10-28 15:12:29 +0100

    feat: see video

--- front/src/components/VideoCard.tsx
+++ front/src/components/VideoCard.tsx
@@ -5,17 +5,30 @@ import {
     CardFooter,
     Typography,
     Button,
-    Tooltip,
     IconButton,
+    Dialog,
+    DialogBody,
+    DialogHeader,
+    DialogFooter,
   } from "@material-tailwind/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 import { Address } from "viem";
-import { useAccount, useReadContract } from "wagmi";
+import { useAccount, useReadContract, useSignMessage } from "wagmi";
    
 import Manager from "../abi/Manager.json";
+import React from "react";
+
+
 
 export function VideoCard({videoId, video}) {
 
   const account = useAccount();
+
+  const [videoUrl, setVideoUrl] = React.useState("");
+  const [openVideo, setOpenVideo] = React.useState(false);
+  const handleOpenVideo = () => setOpenVideo(!openVideo);
+
+
 
   const { data: hasAccess, isLoading: hasAccessLoading, error } = useReadContract({
     address: process.env.NEXT_PUBLIC_CONTRACT_MANAGER_ADDRESS as Address,
@@ -25,12 +38,43 @@ export function VideoCard({videoId, video}) {
       Number(videoId),
       account.address
     ],
-  })
+  });
 
-  console.log(hasAccess);
-  console.log(error);
+  const { signMessage, data: signedMessage } = useSignMessage()
+
+  const auth = async () => {
+    signMessage({ message: 'Prove you are the owner of ' + account.address?.toString() }, {
+      async onSuccess(signature) {
+        console.log("data");
+        console.log(signature);
+
+        const data = {
+          "address": account.address?.toString(),
+          "tokenId": videoId,
+          "signature": signature
+        };
+
+        // Send backend request
+        const uploadRequest = await fetch("/api/load", {
+          method: "POST",
+          body: JSON.stringify(data),
+        });
+
+        const apiData = await uploadRequest.json();
+
+        console.log("apiData result");
+        console.log(apiData);
+        console.log(apiData["url"]);
+
+        setVideoUrl(apiData["url"])
+        handleOpenVideo();
+      }
+    })
+  }
+
 
     return (
+      <>
       <Card className="w-full max-w-[26rem] shadow-lg ml-2 mr-2 mb-4">
         <CardHeader floated={false} color="blue-gray">
           <img
@@ -59,7 +103,7 @@ export function VideoCard({videoId, video}) {
             </Button>
           )}
           {!hasAccessLoading && hasAccess === true && (
-            <Button size="lg" fullWidth={true} variant="outlined">
+            <Button size="lg" fullWidth={true} variant="outlined" onClick={auth}>
               See Video
             </Button>
           )}
@@ -70,5 +114,34 @@ export function VideoCard({videoId, video}) {
           )}
         </CardFooter>
       </Card>
+
+      {/* Dialog for storing the video */}
+      <Dialog size="sm" open={openVideo} handler={handleOpenVideo} className="p-4">
+      
+      <DialogHeader className="relative m-0 block">
+        <Typography variant="h4" color="blue-gray">
+          {video.name}
+        </Typography>
+        <Typography className="mt-1 font-normal text-gray-600">
+        {video.description}
+        </Typography>
+        <IconButton
+          size="sm"
+          variant="text"
+          className="!absolute right-3.5 top-3.5"
+          onClick={handleOpenVideo}
+        >
+          <XMarkIcon className="h-4 w-4 stroke-2" />
+        </IconButton>
+      </DialogHeader>
+      <DialogBody className="space-y-4 pb-6">
+      <video className="h-full w-full rounded-lg" controls>
+        <source src={videoUrl} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      </DialogBody>
+      
+    </Dialog>
+    </>
     );
   }
---
+++ front/src/pages/api/load.ts
@@ -0,0 +1,68 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Address, verifyMessage } from "viem";
+import { readContract } from "@wagmi/core";
+import { config } from "../../wagmi";
+
+import Manager from "../../abi/Manager.json";
+import Video from "../../abi/Video.json";
+import { pinata } from "../../utils/config";
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    if (req.method === 'POST') {
+        console.log("GET function");
+        console.log(req.body);
+
+        const data = JSON.parse(req.body);
+
+        // Verify user signature
+        let verified = await verifyMessage({
+            address: data["address"],
+            message: 'Prove you are the owner of ' + data["address"],
+            signature: data["signature"],
+        })
+
+        if (!verified) {  // Invalid signature
+            res.status(500).json({error: "Internal Server Error"})
+            return;
+        }
+
+        // Check on chain that the given address has access to the video
+        let access = await readContract(config, {
+            address: process.env.NEXT_PUBLIC_CONTRACT_MANAGER_ADDRESS as Address,
+            abi: Manager.abi,
+            functionName: 'access',
+            args: [
+                Number(data["tokenId"]),
+                data["address"]
+            ],
+        });
+        if (!access) {
+            res.status(500).json({error: "Internal Server Error"})
+            return;
+        }
+
+        // Fetch the cid of the data
+        const metadata = await readContract(config, {
+            address: process.env.NEXT_PUBLIC_CONTRACT_VIDEO_ADDRESS as Address,
+            abi: Video.abi,
+            functionName: 'videoMetadata',
+            args: [
+                Number(data["tokenId"])
+            ],
+        });
+        const cid = metadata[3];
+        console.log(cid);
+
+        // Create a pinata link for the video
+        let url = await pinata.gateways.createSignedURL({
+          cid: cid,
+          expires: 3600,
+        });
+
+        console.log(url);
+
+        res.status(200).json({"url": url}) 
+    }
+}
